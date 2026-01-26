5e5dc639cf32cc9311dc02f9cd5f23a32b6cea5f
anuyashkoli <anuyashkoli@gmail.com>
2026-01-27 01:26:53 +0530

    😑

--- src/sandbox/HundredthPrime.java
+++ src/sandbox/HundredthPrime.java
@@ -5,7 +5,7 @@ public class HundredthPrime
    public static void main(String[] args)
    {
       int currentNumber = 2;
-      int maxNumber = 100;
+      int maxNumber = 101;
 
       while (currentNumber <= maxNumber) {
          for (int divisor = 2; divisor <= currentNumber; divisor++) {
@@ -31,4 +31,19 @@ I'll come back to this program later on and would rewrite this !!!
 2) I was doing something with the top level variables and for loop, trying to iterate both divisor and currentNumber parallelly
 3) I have good grasp of if conditionals and for loop, but I have to practise them together and with more complexity
 4) while Loop would take a while
+ */
+
+/*
+Incorrect ❌
+Expected: 541
+Actual:
+2 is a Prime Number
+3 is a Prime Number
+4 is a Composite Number
+.
+97 is a Prime Number
+98 is a Composite Number
+99 is a Composite Number
+100 is a Composite Number
+
  */
