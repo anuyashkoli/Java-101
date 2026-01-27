94d173c019a51c19c067fe3f28198ccfec5af065
anuyashkoli <anuyashkoli@gmail.com>
2026-01-28 00:28:26 +0530

    The Bouncer analogy helped me understand the logic, but I still haven't gotten the logic yet

--- src/sandbox/HundredthPrime.java
+++ src/sandbox/HundredthPrime.java
@@ -4,33 +4,45 @@ public class HundredthPrime {
    public static void main(String[] args) {
       /* Make a code that displays the 100th prime number */
 
-      int startNumber = 2;
-      int maxNumber = 1;
+      // Bouncer
+      int counter = 0;
+      int maxNumber = 99;
+      int currentNumber = 2;
 
-
-      System.out.println("From `while` loop:");
-      while (maxNumber <= 10) {
-
-         System.out.println(startNumber + " x " + maxNumber + " = " + startNumber * maxNumber);
-         maxNumber++;
+      while (counter <= maxNumber) {
+         for (int divisor = 2; divisor <= currentNumber; divisor++) {
+            if (divisor == currentNumber) {
+               // System.out.println(divisor + " is Prime number");
+               counter ++;
+            }
+            else if (currentNumber % divisor == 0){
+               // System.out.println(divisor + " is Composite number");
+               break;
+            }
+         }
+         currentNumber++;
       }
-
-      System.out.println("From `for` loop:");
-      for (int multiplier = 1; multiplier <= 10; multiplier++) {
-
-         System.out.println(multiplier);
-      }
-
+      System.out.println(currentNumber - 1);
    }
-
-
 }
 
 /*
 Not going ahead without solving this:
 Let's Keep a log:
 27-01-2026 - 12:45
- */
+
+27-01-2026 - 19:28
+
+
+Solved:
+[28/01/2026] - 00:24
+I think I have done it, but with a hint, an intrinsic hint
+
+Hint:
+You are painfully close. You have built the engine, but you haven't put it in "Drive" yet, and your Bouncer is a little too thorough.
+I've zeroed in on the core issue: the infinite loop. My code analysis pinpointed that currentNumber isn't incrementing, so it's stuck at 2. This causes an endless prime check.
+
+*/
 
 /*
 I haven't yet got the logic behind it, but the fact that I recreate the code by just looking at the syntax is quite delightful
