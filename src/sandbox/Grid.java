f24403b4529cca2c43b03380d8adf134b378c6f4
anuyashkoli <anuyashkoli@gmail.com>
2026-07-05 13:51:19 +0530

    5x5 Star Grid and 90° Triangle

---
+++ src/sandbox/Grid.java
@@ -0,0 +1,32 @@
+package sandbox;
+
+public class Grid {
+    public static void main(String[] args) {
+        square();
+        triangle();
+    }
+
+    public static void square() {
+        System.out.println("Star Grid");
+        for (int stripe = 1; stripe <= 5; stripe++) {
+            System.out.println("⭐ ⭐ ⭐ ⭐ ⭐");
+        }
+    }
+
+    public static void triangle() {
+        System.out.println("\nRight-angled Triangle");
+        for (int iterator = 1; iterator <= 5; iterator++) {
+            System.out.println();
+            for (int liner = 1; liner <= iterator; liner++) {
+                System.out.print("⭐");
+            }
+        }
+    }
+}
+
+// Phase One: Nested Loop
+/*
+Task 1 (The Grid): Print a 5x5 grid of stars (*).
+Task 2 (The Triangle): Print a right-angled triangle of stars (Line 1 has one star, Line 5 has five stars).
+Task 3 (The Multiplication Table): Print the multiplication tables for numbers 1 through 10. The output should look like a grid.
+ */
