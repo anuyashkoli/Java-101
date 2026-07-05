74957895fb2e998c2ad8d2b69eb383bd16a44d07
anuyashkoli <anuyashkoli@gmail.com>
2026-07-05 10:35:12 +0530

    Clean State

--- src/sandbox/HackerRank.java
+++
@@ -1,9 +0,0 @@
-package sandbox;
-
-public class HackerRank {
-
-    // Range: 25
-    // Square: Should be within the range
-    // Cube: Should be within the range
-    // Square + Cube: Should be within the range
-}
