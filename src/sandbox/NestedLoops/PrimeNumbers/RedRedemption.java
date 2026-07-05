403462a2dd4013cd1d5e0b835fee07967fa014e7
anuyashkoli <anuyashkoli@gmail.com>
2026-07-05 16:24:59 +0530

    Part of learning Java is learning how to write less of it

--- src/sandbox/NestedLoops/PrimeNumbers/RedRedemption.java
+++ src/sandbox/NestedLoops/PrimeNumbers/RedRedemption.java
@@ -16,3 +16,10 @@ public class RedRedemption {
         return true;
     }
 }
+
+/*
+In isPrime() in Redemption.java,
+for loop runs two if checks on every single pass. For 541, computer performs over 1,000 checks.
+
+The RedRedemption, just changes the for loop to increment divisior upto divisor < number, instead of divisor <= number
+*/
