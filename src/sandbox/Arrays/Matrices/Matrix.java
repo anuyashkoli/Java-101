d2567e71be9bed49ebb7d1598960a5accd647ca8
anuyashkoli <anuyashkoli@gmail.com>
2026-07-06 11:55:40 +0530

    Task 1 - Print Entire Grid in Grid Layout with space

--- src/sandbox/Arrays/Matrices/Matrix.java
+++ src/sandbox/Arrays/Matrices/Matrix.java
@@ -2,13 +2,37 @@ package sandbox.Arrays.Matrices;
 
 public class Matrix {
 
-    public static void main(String[] Args) {
-        int [][] matrix = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9},
-        };
+    static int [][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9},
+    };
+    static int arraySize = matrix.length - 1;
 
-        System.out.println(matrix[2][1]);
+    public static void main(String[] Args) {
+        thePrinter();
+    }
+
+    public static void thePrinter () {
+        for (int forRow = 0; forRow <= arraySize; forRow ++) {
+            System.out.println();
+            for (int forColumn = 0; forColumn <= arraySize; forColumn++) {
+                System.out.print(matrix[forRow][forColumn]);
+                System.out.print("\t");
+            }
+        }
     }
 }
+
+// Tasks
+/*
+Task 1 (The Printer):
+Write a nested for loop (an outer loop for rows, inner loop for columns) to print the entire grid exactly as it looks above, with spaces between the numbers.
+
+Task 2 (The Diagonal):
+Write a new loop that goes through the grid but only prints the numbers on the diagonal line from top-left to bottom-right (1, 5, 9).
+(Hint: What is the relationship between the row number and the column number for those specific spots?)
+
+Task 3 (The Hacker):
+Write code to replace the number 5 in the very center of the grid with the number 99.
+Then, call your Task 1 printer loop again to prove the board was updated.*/
