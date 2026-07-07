b95ac48745c787f9ac8db9e9e96754012f249943
anuyashkoli <anuyashkoli@gmail.com>
2026-07-07 15:31:23 +0530

    Task 1 - Canvas(): - Initialises a 3x3 Grid - Ffills evrything with '-' - Encapsulated in Square Bracket [-]

---
+++ src/sandbox/TicTacToe/Warmup.java
@@ -0,0 +1,36 @@
+package sandbox.TicTacToe;
+
+import java.util.Scanner;
+
+public class Warmup {
+    static Scanner ask = new Scanner(System.in);
+    static char[][] board = new char[3][3];
+
+    public static void main (String[] Args) {
+        Canvas();
+    }
+
+    public static void Canvas () {
+        for (int cellRow = 0; cellRow <= board.length - 1; cellRow++ ) {
+            System.out.println();
+            for (int cellColumn = 0; cellColumn <=board.length - 1; cellColumn ++) {
+                System.out.print("  ");
+                board[cellRow][cellColumn] = '-';
+                System.out.print("[" + board[cellRow][cellColumn] + "]");
+            }
+        }
+    }
+}
+
+// TASKs
+/*
+Task 1 (The Canvas):
+Initialize a 3x3 grid and fill every empty coordinate with a dash ('-').
+
+Task 2 (The Printer):
+Create a reusable method named printBoard() that prints the current state of your grid to the console in a visually clean 3x3 format.
+
+Task 3 (The First Move):
+Prompt the user to enter a row and a column.
+Place an 'X' at that exact coordinate on the board, and reprint the board to prove the move was successful.
+*/
