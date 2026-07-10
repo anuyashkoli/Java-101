937338d27c598d2585b26b72e5a2efcd038c072a
anuyashkoli <anuyashkoli@gmail.com>
2026-07-10 21:21:11 +0530

    Implement - Infinite List : Empty String ArrayList<> - Ennrollment: Trapped user in while loop; loop breaks if user enters exit - Expansion: Every entered name is added in the ArrayList<> - Bouncer: Loop does break if user enters exit, converts user input into lowercase to handle any type of input

---
+++ src/sandbox/ArrayLists/TheRoaster.java
@@ -0,0 +1,37 @@
+package sandbox.ArrayLists;
+
+import java.awt.desktop.SystemEventListener;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TheRoaster {
+    public static void main(String[] Args) {
+        boolean isRunning = true;
+        Scanner ask = new Scanner(System.in);
+        ArrayList<String> customerNames = new ArrayList<String>(); // Empty ArrayList
+
+        while (isRunning) {
+            System.out.print("\nEnter a new customer name: ");
+            String customerName = ask.nextLine();
+            customerNames.add(customerName);
+            System.out.print("Current Customers: ");
+            for (int iterator = 0; iterator < customerNames.size(); iterator++ ) {
+                System.out.print("[");
+                System.out.print(customerNames.get(iterator));
+                System.out.print("]");
+            }
+            if (customerName.equalsIgnoreCase("exit")) {
+                isRunning = false;
+            }
+        }
+    }
+}
+
+/*Acceptance Criteria:
+
+The Infinite List: Create a Scanner and an empty ArrayList of type String.
+The Enrollment Loop: Trap the user in a while loop that asks them to "Enter a new customer name (or type 'EXIT' to stop):".
+The Expansion: Every time they type a name, add it to the ArrayList.
+The Bouncer: If they type 'EXIT', the loop must immediately stop (break).
+The Grand Reveal: After the loop ends, write a for loop that uses .size() and .get() to print out the final roster of all enrolled customers.
+ */
