3b09e716892ea6037fde8bba46f4bd7744a95dee
anuyashkoli <anuyashkoli@gmail.com>
2026-07-10 23:51:25 +0530

    Exception Handling { InputMismatchExceptionHandling }

---
+++ src/sandbox/ExceptionHandling/InputMismatchExceptionHandling.java
@@ -0,0 +1,43 @@
+package sandbox.ExceptionHandling;
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class InputMismatchExceptionHandling {
+
+    static Scanner ask = new Scanner(System.in);
+    static boolean isRunning = true;
+
+    public static void main(String[] Args) {
+        ageVerification();
+    }
+
+    public static void ageVerification() {
+        while (isRunning) {
+            try {
+                System.out.println("Enter your age: ");
+                int userAge = ask.nextInt();
+                if (userAge >= 18) {
+                    System.out.println("Your an Adult");
+                } else {
+                    System.out.println("Your are minor");
+                }
+                isRunning = false;
+            } catch (InputMismatchException e) {
+                System.out.println("Enter Number");
+                ask.nextLine();
+            }
+        }
+        ask.close();
+    }
+}
+
+/*
+The Brief: Build a secure age-verification terminal.
+
+Acceptance Criteria: Ask the user to enter their age as a number.
+If they type a word instead of a number, the program must not crash.
+It must catch the error, print "Invalid input, please type a number," and ask them again until they get it right.
+
+(Hint: You will need to research try and catch blocks).
+ */
---
+++ src/sandbox/HashMaps/AadhaarDirectory.java
@@ -0,0 +1,12 @@
+package sandbox.HashMaps;
+
+public class AadhaarDirectory {
+
+}
+
+/*
+The Brief: Build a high-speed lookup directory.
+
+Acceptance Criteria: Create a HashMap where the Key is an Integer (Account Number) and the Value is a BankAccount object.
+Add three accounts. Ask the user to type an Account Number, and instantly print the details of that specific account without writing a single for loop.
+ */
