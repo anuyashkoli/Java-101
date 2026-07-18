fa1efca110ad3d27a5822b023ed3ae856b680c4a
anuyashkoli <anuyashkoli@gmail.com>
2026-07-18 16:03:57 +0530

    Database is set

--- src/sandbox/Products/TerminalShop/TerminalShop.java
+++ src/sandbox/Products/TerminalShop/TerminalShop.java
@@ -1,8 +1,24 @@
 package sandbox.Products.TerminalShop;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TerminalShop {
     public static void main(String[] Args) {
 
+    }
+
+    public static void theDatabase() {
+        Map<Integer, Product> productHashMap = new HashMap<>();
+
+        //Preloading
+        Product HP = new Product("HP Laptop", 50000, 10);
+        Product Lenovo = new Product("Lenovo Laptop", 55000, 10);
+        Product Dell = new Product("Dell Laptop", 60000, 10);
+
+        productHashMap.put(1, HP);
+        productHashMap.put(2, Lenovo);
+        productHashMap.put(3, Dell);
     }
 }
 
@@ -12,7 +28,7 @@ public class TerminalShop {
 The Execution: TerminalShop.java
 
 This file is your front-end UI and database.
-The Database: Create a HashMap<Integer, Product>. The Integer key will act as the "Item ID" (e.g., Key 1 = A Laptop, Key 2 = Coffee). Load it with at least 3 distinct products.
+✔️ The Database: Create a HashMap<Integer, Product>. The Integer key will act as the "Item ID" (e.g., Key 1 = A Laptop, Key 2 = Coffee). Load it with at least 3 distinct products.
 The Main Loop: Trap the user in a while loop with three distinct options:
 View Inventory: Use a loop to print every Item ID, its Name, Price, and current Stock.
 Buy an Item: Ask the user for the Item ID they want to buy. Use .containsKey() to make sure the ID exists. If it does, trigger the buy method on that specific object.
